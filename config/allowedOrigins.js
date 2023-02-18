028a75cf84e7824de9e04cd5ee775e3fe235e790
z3phyron <z3phyronsnides@gmail.com>
2023-02-18 04:08:41 +0100

    allow origin from client

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'http://localhost:3500'
+    'http://localhost:3500',
+    'https://portfolio-beta-nine-94.vercel.app/'
 ]
 
 module.exports = allowedOrigins
