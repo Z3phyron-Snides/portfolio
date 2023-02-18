e9f89ce7a5c97693a3f2ba827be3e437fb17ccfd
z3phyron <z3phyronsnides@gmail.com>
2023-02-18 04:16:23 +0100

    ..

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:3500',
-    'https://portfolio-beta-nine-94.vercel.app/'
+    'https://portfolio-beta-nine-94.vercel.app'
 ]
 
 module.exports = allowedOrigins
